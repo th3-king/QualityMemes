1b9f614215a03eaf9f9a816750221cb1fdff38a5
Dan Scott <Dan@Dans-MacBook-Air.local>
2016-07-18 18:42:48 +0800

    added block object
    Block object added with collision detection and minor code changes to
    neaten up code

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,40 +1,50 @@
 //global variables
-	//initialisers
-	var currentScene = "main";
-	var screenWidth = 0;
-	var screenHeight = 0;
-	var levelInterval;
-	var startScreenInterval;
-	var paused = false;
-	var createdClouds = false;
-	var initialRun = true;
-	var gameplayFreeze = false;
-	window.mario = new Mario(screenHeight/15, screenHeight*3/5, screenHeight*1/10, screenHeight*3/20, screenWidth/200, marioTexture);
+//initialisers
+var currentScene = "main";
+var screenWidth = 0;
+var screenHeight = 0;
+var levelInterval;
+var startScreenInterval;
+var paused = false;
+var createdClouds = false;
+var initialRun = true;
+var gameplayFreeze = false;
+window.mario = new Mario(screenHeight/15, screenHeight*3/5, screenHeight*1/10, screenHeight*3/20, screenWidth/200, marioTexture);
 
-	//audio setup
-	function play() {
-		document.getElementById('themeSong').play();
-	}
-	function pause() {
-		document.getElementById('themeSong').pause();
-	}
+//audio setup
+function play() {
+	document.getElementById('themeSong').play();
+}
+function pause() {
+	document.getElementById('themeSong').pause();
+}
 
-	//arrays
-	var levelOneSprites = [];
-	var clouds = [];
+//arrays
+var levelSprites = [];
+var levelBlocks
+var clouds = [];
 
-	//game properties
-	var levelLoaded = false;
-	var xPositionInLevel = 0;
-	var xPositionInLevelMultiplier = 0;
-	var lives = 3;
-	var jump = false;
-	var moveLeft = false;
-	var moveRight = false;
-	var moveDown = false;
-	var moveUp = false;
+//game properties
+var levelLoaded = false;
+var xPositionInLevel = 0;
+var xPositionInLevelMultiplier = 0;
+var lives = 3;
+var moveLeft = false;
+var moveRight = false;
+var moveDown = false;
+var moveUp = false;
 
-	//Declare Characters for level
-	function declareLevelOneSprites() {
-	levelOneSprites[0] = new Sprite(screenWidth*3/4, screenHeight*2/3, screenHeight/12, screenHeight/12, screenWidth/500, gooba);
-	}
+//Declare Characters for level
+function declareLevelOneSprites() {
+	if (levelLoaded == false){
+		levelSprites = [];
+		levelSprites[0] = new Sprite(screenWidth*3/4, screenHeight*2/3, screenHeight/12, screenHeight/12, screenWidth/500, gooba);
+	};
+};
+
+function declareLevelOneBlocks() {
+	if (levelLoaded == false){
+		levelBlocks = [];
+		levelBlocks[0] = new Block(screenWidth * 1.5, screenHeight*5/8, screenHeight/8, screenHeight/8, groundTexture);
+	};
+};
