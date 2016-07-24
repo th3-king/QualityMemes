52f5bedd7cf39994a101fa184671c4f25f9887cc
Dan Scott <Dan@Dans-MacBook-Air.local>
2016-07-24 12:40:24 +0800

    Added comment's to explain function use
    comments have been added to main function file and a the texture added
    to “additionalTextures” is the template for level one

--- javascript/mainFunctions.js
+++ javascript/mainFunctions.js
@@ -1,9 +1,12 @@
+//functions which are used to manipulate variables
+//creates level interval if none exist for specific scene
 function createLevelInterval(functionOfScene){
 		if (levelInterval == undefined || levelInterval == 0){
 	    levelInterval = setInterval(functionOfScene, 15);
 	  }
 }
 
+//creates a multiplier for an object which is used for screen resizing
 function createScreenChangeMultiplier(object) {
 		object.multiplierX = object.x/screenWidth;
 		object.multiplierY = object.y/screenHeight;
@@ -14,6 +17,7 @@ function createScreenChangeMultiplier(object) {
 		object.multiplierGravity = object.gravity/screenHeight;
 }
 
+//creates a multiplier for an array of objects which is used for screen resizing
 function createScreenChangeMultiplierArray(arrayOfObjects) {
 	for(var i = 0; i < arrayOfObjects.length; i++){
 		createScreenChangeMultiplier(arrayOfObjects[i]);
@@ -21,6 +25,8 @@ function createScreenChangeMultiplierArray(arrayOfObjects) {
 	}
 }
 
+/* uses multiplier created from createScreenChangeMultiplier
+function and uses to reposition objects correctly */
 function updateObjectToScreenChange(object) {
 		object.x = object.multiplierX * screenWidth;
 		object.y = object.multiplierY * screenHeight;
@@ -31,6 +37,7 @@ function updateObjectToScreenChange(object) {
 		object.gravity = object.multiplierGravity*screenHeight;
 }
 
+/*does the same as updateObjectToScreenChange, except for array of objects*/
 function updateObjectToScreenChangeArray(arrayOfObjects) {
 	for(var i = 0; i < arrayOfObjects.length; i++){
 		updateObjectToScreenChange(arrayOfObjects[i]);
@@ -38,6 +45,8 @@ function updateObjectToScreenChangeArray(arrayOfObjects) {
 	}
 }
 
+/*checks if paused, if it is it freezes the animation and
+draws the paused box waiting for user input*/
 function pauseScene(){
 	if(paused == true){
 		gameplayFreeze = true;
@@ -47,6 +56,8 @@ function pauseScene(){
 	}
 }
 
+/*clears the current interval and resets all necessary
+ level variables so it can progress to next scene*/
 function refreshLevelAndGoToScene(scene){
   clearInterval(levelInterval);
   levelInterval = 0;
@@ -60,6 +71,8 @@ function refreshLevelAndGoToScene(scene){
   }
 }
 
+/*specifically for main scene but stops main scene interval
+allows you to move to new scene*/
 function refreshMainScene(){
   clearInterval(startScreenInterval);
   startScreenInterval = 0;
@@ -71,6 +84,8 @@ function groundDetectCollision(){
 
 }
 
+/*detects if mario collides with enemy, it then checks whether he
+got hit or squashed the enemy and does specific actions for either*/
 function enemyDetectCollision(marioObject, enemyObject){
   if(marioObject.x + marioObject.width >= enemyObject.x && marioObject.x <= enemyObject.x + enemyObject.width && marioObject.y + marioObject.height >= enemyObject.y && marioObject.y <= enemyObject.y + enemyObject.height){
     if(marioObject.y + marioObject.height <= enemyObject.y + enemyObject.height/8){
@@ -93,15 +108,13 @@ function test(){
   console.log("test");
 }
 
+/*set's the curret sprite removed to true*/
 function removeSprite(sprite){
   sprite.removed = true;
 }
 
+/*returns the X position used for the ground image so it moves
+with the players movement*/
 function groundXPosition() {
 	return -(xPositionInLevel % (screenHeight/8));
 };
-
-Object.defineProperty(Array.prototype, "next", {
-    value: function() { return this[++this.current]; },
-    enumerable: false
-});
